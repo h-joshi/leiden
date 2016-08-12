294dc7b25eb1a55d19a98861afc54c9ab83a2a1f
Abdul Zreika <azre6702@uni.sydney.edu.au>
2016-08-12 22:50:46 +1000

    formatting fixed for ExAC data; final output stored

--- leiden_overlay/processing/process_data.py
+++ leiden_overlay/processing/process_data.py
@@ -2,7 +2,7 @@ import re
 
 input_gene = "DYSF"
 
-gene_vep_file = open("../dat/" + input_gene + "_VEP_FINAL.txt")
+gene_vep_file = open("../dat/" + input_gene + ".vep.txt")
 lines = [line.rstrip('\n').split('\t') for line in gene_vep_file]
 gene_vep_file.close()
 
@@ -13,24 +13,48 @@ while lines[i][0][0]=='#':
 col_names = lines[i-1]
 col_names[0] = col_names[0][1:]
 rows = lines[i:]
-print col_names
+#print col_names
 extra_index = col_names.index("Extra")
 
 output_file = open("../dat/" + input_gene + "_final_output.txt", "w")
+concise_file = open("../dat/" + input_gene + "_concise_output.txt", "w")
 var_name_ind = col_names.index("Uploaded_variation")
 location_ind = col_names.index("Location")
 conseq_ind = col_names.index("Consequence")
 
-first_row_out = ("%s\t%s\t%s\t%s\n")%("Uploaded_variation", "Location", "Consequence", "Result")
+first_row_out = ("%s\t%s\t%s\t%s\n")%("Uploaded_variation", "Location", "Consequence", "Allele_frequency")
+
+concise_file.write(first_row_out)
 output_file.write(first_row_out)
+
 for line in rows:
     extra_data = line[col_names.index("Extra")].split(';')
-    freq_data = next((x for x in extra_data if "ExAC_Adj_MAF=" in x), None)
-    print line[var_name_ind], line[location_ind], line[conseq_ind], freq_data
+    freq_data = next((x.split('=')[1] for x in extra_data if "ExAC_Adj_MAF=" in x), None)
+
+    add_to_concise = True
+    #get the actual change from freq_data - TODO: CHECK THIS WITH HIMANSHU
+    var_name = line[var_name_ind]
+    if '>' in var_name and freq_data:
+        tmp_freq_data = freq_data.replace(',', ':')
+        new_base = var_name[-1]
+        res_list = tmp_freq_data.split(':')
+        try:
+            main_ind = res_list.index(new_base)
+            freq_data = "%.9f"%(float(res_list[main_ind+1]))
+        except:
+            freq_data = "No relevant ExAC data"
+            add_to_concise = False
+
+    #print line[var_name_ind], line[location_ind], line[conseq_ind], freq_data
     if freq_data == None:
-        freq_data = "NULL"
+        freq_data = "No ExAC data for variant"
+        add_to_concise = False
+
     row_to_add = ("%s\t%s\t%s\t%s\n")%(line[var_name_ind], line[location_ind], line[conseq_ind], freq_data)
     output_file.write(row_to_add)
+    if add_to_concise:
+        concise_file.write(row_to_add)
     #output_file.write("%s\t%s\t%s\t%s\t%s\t%s
 
 output_file.close()
+concise_file.close()
--- leiden_overlay/processing/run_script.sh
+++ leiden_overlay/processing/run_script.sh
@@ -1,10 +1,22 @@
 #! /usr/bin/env sh
 
+#USE: ./run_script DYSF
+
+echo "Gathering data from Leiden..."
 # create the initial files using andrew's scripts
 python ./../../bin/extract_data.py --leiden_url http://www.dmd.nl/nmdb2/ -o ../dat --gene_list $1
 echo "../dat/$1.txt" > file_list.txt
+echo "Generating VCF file..."
 python ./../../bin/generate_annotated_vcf.py -f file_list.txt
 
-# use VEP to gather all the data together
-perl ~/ensembl-tools-release-84/scripts/variant_effect_predictor/variant_effect_predictor.pl -i ../dat/$1.vcf --cache --port 3337 --maf_exac --force_overwrite -o ./../dat/$1\_VEP_FINAL.txt
 
+# use VEP to gather all the data together
+echo "Bringing data in from VEP and ExAC..."
+perl ~/ensembl-tools-release-84/scripts/variant_effect_predictor/variant_effect_predictor.pl -i ../dat/$1.vcf --cache --port 3337 --maf_exac --force_overwrite -o ./../dat/$1.vep.txt
+
+# form the final result output
+echo "Generating result file..."
+python process_data.py
+
+# done!
+echo "Result file generated for $1!"
